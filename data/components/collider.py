9f3b21a4d1c7c94387629174b4b853d9f55ab208
bitstuffing <eduardo.higuero@hotmail.com>
2020-07-06 11:52:05 +0200

    transparent collinder

--- data/components/collider.py
+++ data/components/collider.py
@@ -5,7 +5,8 @@ class Collider(pg.sprite.Sprite):
     def __init__(self, x, y, width, height):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((width, height)).convert()
-        self.image.fill(RED)
+        self.image.set_alpha(0)
+        #self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
